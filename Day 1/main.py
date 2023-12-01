81dda68d5a5e78a4a5ccfc8d86630b4bc162862d
Denno20 <dennisonethan@yahoo.co.uk>
2023-12-01 17:24:56 +0000

    Commented code

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,3 +1,41 @@
+def get_index(digit):
+    return digit.get("index")
+
+#Borrowed this from https://stackoverflow.com/questions/3873361/finding-multiple-occurrences-of-a-string-within-a-string-in-python
+def findall(text, sub):
+    """Return all indices at which substring occurs in text"""
+    return [
+        index
+        for index in range(len(text) - len(sub) + 1)
+        if text[index:].startswith(sub)
+    ]
+
+
+#Does what it says really
+def convert_word_to_number(word):
+    match word:
+        case "one":
+            return 1
+        case "two":
+            return 2
+        case "three":
+            return 3
+        case "four":
+            return 4
+        case "five":
+            return 5
+        case "six":
+            return 6
+        case "seven":
+            return 7
+        case "eight":
+            return 8
+        case "nine":
+            return 9
+        case _:
+            return int(word)
+
+
 def main():
     sum = 0
     numbers = [
@@ -23,12 +61,17 @@ def main():
 
     with open("values.txt", "r") as f:
         for line in f.readlines():
+            #Define an empty dictionary for every line
             digits = {}
+            #Loop through the number array
             for val in numbers:
+                #If the line contains a number (string or int form)
+                #Use regex to find all overlapping entries
                 if line.find(str(val)) != -1:
                     for i in findall(line, str(val)):
                         digits[i] = convert_word_to_number(str(val))
-        
+
+            #Sort digits by index value and then add the first and last
             sortedDigits = sorted(digits.items(), key=lambda t: t[0])
             calibrationValue = int(str(sortedDigits[0][-1]) + str(sortedDigits[-1][-1]))
 
@@ -36,41 +79,6 @@ def main():
     
     print(f"The final sum: {sum}")
 
-def get_index(digit):
-    return digit.get("index")
-
-#Borrowed this from https://stackoverflow.com/questions/3873361/finding-multiple-occurrences-of-a-string-within-a-string-in-python
-def findall(text, sub):
-    """Return all indices at which substring occurs in text"""
-    return [
-        index
-        for index in range(len(text) - len(sub) + 1)
-        if text[index:].startswith(sub)
-    ]
-
-
-def convert_word_to_number(word):
-    match word:
-        case "one":
-            return 1
-        case "two":
-            return 2
-        case "three":
-            return 3
-        case "four":
-            return 4
-        case "five":
-            return 5
-        case "six":
-            return 6
-        case "seven":
-            return 7
-        case "eight":
-            return 8
-        case "nine":
-            return 9
-        case _:
-            return int(word)
-
+#Entry point for program
 if __name__=="__main__":
     main()
